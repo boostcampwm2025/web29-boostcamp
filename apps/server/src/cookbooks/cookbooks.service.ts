0a219faa008c800c46bd9aa5490143b14c964265
vvzvvv <tmdgusv@gmail.com>
2026-01-15 18:52:31 +0900

    refactor: 쿡북 상세 조회 시 DTO 반환으로 변경

--- apps/server/src/cookbooks/cookbooks.service.ts
+++ apps/server/src/cookbooks/cookbooks.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cookbook } from 'src/entities/cookbook.entity';
+import { CookbookDetailResponseDto } from './dto/cookbook-detail-response.dto';
 
 @Injectable()
 export class CookbooksService {
@@ -10,7 +11,9 @@ export class CookbooksService {
     private readonly cookbookRepository: Repository<Cookbook>,
   ) {}
 
-  async findByCookbookId(cookbookId: number): Promise<Cookbook> {
+  async findByCookbookId(
+    cookbookId: number,
+  ): Promise<CookbookDetailResponseDto> {
     const cookbook = await this.cookbookRepository.findOne({
       where: { id: cookbookId },
       relations: ['tags', 'cookbook_problems', 'cookbook_problems.problem'],
@@ -20,6 +23,18 @@ export class CookbooksService {
       throw new NotFoundException(`Cookbook with ID ${cookbookId} not found`);
     }
 
-    return cookbook;
+    return {
+      id: cookbook.id,
+      title: cookbook.title,
+      description: cookbook.description,
+      tags: cookbook.tags.map((tag) => tag.name),
+      problems: cookbook.cookbook_problems
+        .sort((a, b) => a.order_number - b.order_number)
+        .map((cp) => ({
+          id: cp.problem.id,
+          title: cp.problem.title,
+          order_number: cp.order_number,
+        })),
+    };
   }
 }
