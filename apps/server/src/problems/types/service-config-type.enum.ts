e8fef09313c8e3692abfc2ab74e878fa6d3f35da
shahhenshah <mrsouthwestboy@gmail.com>
2026-01-20 20:38:34 +0900

    feat: aws 서비스 타입 추가

--- apps/server/src/problems/types/service-config-type.enum.ts
+++ apps/server/src/problems/types/service-config-type.enum.ts
@@ -1,23 +1,38 @@
-import { IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 
 // TODO: 각 서비스별 Config 타입 구체화하기
 
 export class EC2Config {
   @IsString()
   id: string;
+
+  @IsString()
+  name: string;
+
   @IsString()
   vpcId: string;
 
   @IsString()
+  vpcName: string;
+
+  @IsString()
   subnetId: string;
+  @IsString()
+  subnetName: string;
 
   @IsString()
   instanceType: string;
+
+  @IsString({ each: true })
+  securityGroups: string[]; // Security Group names
 }
 
 export class VPCConfig {
   @IsString()
   id: string;
+
+  @IsString()
+  name: string;
 
   @IsString()
   cidrBlock: string;
@@ -28,8 +43,73 @@ export class SubnetConfig {
   id: string;
 
   @IsString()
+  name: string;
+
+  @IsString()
   vpcId: string;
+
+  @IsString()
+  vpcName: string;
 
   @IsString()
   cidrBlock: string;
 }
+
+export class SecurityGroupsConfig {
+  @IsString()
+  id: string;
+  @IsString()
+  vpcId: string;
+  @IsString()
+  vpcName: string;
+  @IsString()
+  name: string;
+}
+
+export class S3Config {
+  @IsString()
+  id: string;
+
+  @IsString()
+  name: string;
+}
+
+export class InternetGatewayConfig {
+  @IsString()
+  id: string;
+  @IsString()
+  vpcId: string;
+  @IsString()
+  name: string;
+}
+
+export type GatewayTypes = InternetGatewayConfig;
+
+export class RouteTableEntry {
+  @IsString()
+  destinationCidr: string;
+  @IsString()
+  targetGatewayId: string;
+}
+
+export class RouteTableConfig {
+  @IsString()
+  id: string;
+  @IsString()
+  vpcId: string;
+  @IsString()
+  name: string;
+  @IsArray()
+  routes: RouteTableEntry[];
+  @IsString({ each: true })
+  associations: string[]; // Subnet IDs
+}
+
+export type ServiceConfigTypes =
+  | EC2Config
+  | VPCConfig
+  | SubnetConfig
+  | RouteTableConfig
+  | SecurityGroupsConfig
+  | S3Config
+  | InternetGatewayConfig;
