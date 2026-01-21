3da1deb6a5602df0016fc50eeb3902cea84c3115
zooyaam <zooyaa11@naver.com>
2026-01-21 23:33:37 +0900

    feat: 레이아웃 수정

--- apps/web/src/app/(guest)/problems/[id]/page.tsx
+++ apps/web/src/app/(guest)/problems/[id]/page.tsx
@@ -1,5 +1,9 @@
+import { FeedbackDetailCard } from '../components/feedback-detail-card'
 import ProblemDetailClient from './problem-detail-client'
 
+import React from 'react'
+
+import { Button } from '@/components/ui/button'
 import { getProblemData } from '@/lib/problem/get-problem-data'
 
 interface ProblemDetailPageProps {
@@ -11,6 +15,27 @@ interface ProblemDetailPageProps {
   }>
 }
 
+const mockFeedbackMessages = [
+  {
+    service: 'S3',
+    field: '',
+    message:
+      '버킷 이름은 전역적으로 고유해야 합니다. 다른 이름을 시도해 주세요.',
+  },
+  {
+    service: 'S3',
+    field: 'blockPublicAccess',
+    message:
+      '퍼블릭 액세스 차단 설정이 권장됩니다. 보안 위험을 줄이기 위해 모든 퍼블릭 액세스를 차단하는 것이 좋습니다.',
+  },
+  {
+    service: 'S3',
+    field: 'encryption',
+    message:
+      '버킷 암호화를 활성화하여 저장된 데이터를 보호하는 것이 좋습니다. SSE-S3 또는 SSE-KMS를 사용하는 것을 고려해 보세요.',
+  },
+]
+
 export default async function ProblemDetailPage({
   params,
   searchParams,
@@ -20,5 +45,19 @@ export default async function ProblemDetailPage({
 
   const problemData = await getProblemData(id)
 
-  return <ProblemDetailClient problemData={problemData} />
+  return (
+    <React.Fragment>
+      <ProblemDetailClient problemData={problemData} />
+
+      <div className="relative h-full">
+        <div className="sticky top-24 space-y-4">
+          <Button className="ml-auto block">제출하기</Button>
+
+          <div className="rounded-xl border p-4">다이어그램</div>
+
+          <FeedbackDetailCard feedback={mockFeedbackMessages} />
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
