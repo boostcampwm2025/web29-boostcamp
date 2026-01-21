a32cb88a0d98291b8a87effe724a28dd111e8dbb
vvzvvv <tmdgusv@gmail.com>
2026-01-21 17:59:11 +0900

    refactor: *에서 명시적 export 로 변경

--- apps/web/src/types/aws-services/ec2/ec2-instance-create/index.ts
+++ apps/web/src/types/aws-services/ec2/ec2-instance-create/index.ts
@@ -1,2 +1,5 @@
-export * from './ec2-form-data.types'
-export * from './ec2-config.types'
+export type { EC2InstanceFormData } from './ec2-form-data.types'
+export type {
+  EC2SectionProps,
+  EC2WithSetValueSectionProps,
+} from './ec2-config.types'
